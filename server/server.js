5dddf14dc47af28e18064d35b798ab9c978c6d64
hafidz <hafidzzpermana09@gmail.com>
2017-12-31 16:30:59 +0700

    Commenting code for future reference

--- server/server.js
+++ server/server.js
@@ -1,40 +1,58 @@
 process.env.NODE_ENV !== "development" && require("dotenv").config()
 
-// Express Depedecies
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const mongoose = require('mongoose');
-const jwt = require("jsonwebtoken");
+// -----------------------------------------------------------------------------
+// NODE, EXPRESS, MIDDLEWARE MODULES
+// -----------------------------------------------------------------------------
 
-mongoose.connect(process.env.DATABASE, {useMongoClient:true});
-//mongoose.connect('mongodb://localhost/qna', {useMongoClient:true});
-mongoose.Promise = global.Promise;
+// Express Dependencies
+const express       = require('express');       // define express
+const bodyParser    = require('body-parser');   // extract the entire body portion of an incoming request stream and exposes it on req.body
+const cookieParser  = require("cookie-parser"); // IDK yet
+const logger        = require('morgan');        // log status server to CLI
+const cors          = require('cors');          // IDK yet
+const path          = require("path");          // Node internal module: Joins all given path segments together using the platform specific separator as a delimiter, then normalizes the resulting path.
+const mongoose      = require('mongoose');      // ODM for MongoDB
+const app           = express()                 // Init Express
+
+// JSON Web Tokens
+const jwt           = require("jsonwebtoken");  // Auth JWT
+
+// -----------------------------------------------------------------------------
+// APP MODULES
+// -----------------------------------------------------------------------------
 
 // Routes
 const apiQuestions = require('./routes/api.questions');
 const apiUsers = require('./routes/api.users');
 const apiAuth = require("./routes/api.auth");
 
-const app = express()
+// -----------------------------------------------------------------------------
+// APP CONFIGURATION
+// -----------------------------------------------------------------------------
 
-app.use(express.static(path.join(__dirname, "public")))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
-app.use(cookieParser())
-app.use(logger('dev')); 
-app.use(cors());    
+// EXPRESS
+app.use(express.static(path.join(__dirname, "public"))) // serve static files
+app.use(bodyParser.urlencoded({ extended: true }))      // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                             // parse application/json
+app.use(cookieParser())                                 // IDK yet
+app.use(logger('dev'));                                 // log server status response to CLI 
+app.use(cors());                                        // enable cors for all origins
+
+//MONGO
+mongoose.Promise = global.Promise;                              // Native Node.js promise
+mongoose.connect(process.env.DATABASE, {useMongoClient:true});  // connect server to mongodb
+
+// -----------------------------------------------------------------------------
+// REGISTER ROUTES
+// -----------------------------------------------------------------------------
 
 app.use('/api/questions', apiQuestions);
 app.use('/api/users', apiUsers)
 app.use("/auth", apiAuth)
 
-app.get('/', function (req, res) {
-  res.send('TOPPPP !');
-})
+// -----------------------------------------------------------------------------
+// RUN THE APP
+// -----------------------------------------------------------------------------
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080!');
